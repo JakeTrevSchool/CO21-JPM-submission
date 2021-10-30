9792fd0d59452b12bd7c01894e36ce30ba555fec
AnimatorOfSouls <fantailfishy@gmail.com>
2021-10-30 14:23:53 +0100

    Fixed types to account for pokemon with multiple types

--- TwoAPIs/main.py
+++ TwoAPIs/main.py
@@ -15,13 +15,19 @@ bot = commands.Bot(command_prefix=",p ", intents=discord.Intents.default())
 async def view(ctx, pname):
 	p = pb.pokemon(pname.lower())
 	
+	types = "`"
+	for i in range(len(p.types)):
+		types += p.types[i].type.name+"/"
+	types = types[:-1]
+	types += "`"
+	
 	abilities = ""
 	for i in range(len(p.abilities)):
 		abilities += str(i+1)+" - `"+p.abilities[i].ability.name+"`\n"
 	
 	embed = discord.Embed(title=f"{p.id} - {p.name}", color=0xfcfc14)
 	embed.set_thumbnail(url=p.sprites.front_default)
-	embed.add_field(name="General Info", value=f"Type: `{p.types[0].type.name}`\nHeight: `{p.height}`\tWeight: `{p.weight}`", inline=False)
+	embed.add_field(name="General Info", value=f"Type: `{types}`\nHeight: `{p.height}`\tWeight: `{p.weight}`", inline=False)
 	embed.add_field(name="Stats", value=f"HP: `{p.stats[0].base_stat}`\nATK/DEF: `{p.stats[1].base_stat}/{p.stats[2].base_stat}`\nSP ATK/SP DEF: `{p.stats[3].base_stat}/{p.stats[4].base_stat}`\nSpeed: `{p.stats[5].base_stat}`", inline=False)
 	embed.add_field(name="Abilities", value=f"{abilities}", inline=False)
 	await ctx.message.channel.send(embed=embed)
