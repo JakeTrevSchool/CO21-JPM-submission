615e8a0c9be21360325a004ae47436a85fc28d55
AnimatorOfSouls <fantailfishy@gmail.com>
2021-10-30 14:15:22 +0100

    Uploaded TwoAPIs task

---
+++ TwoAPIs/README.md
@@ -0,0 +1,15 @@
+# PokéView
+A discord bot that displays information about a requested Pokémon in a nice format.  
+Made for GUTechSociety Code Olympics 2021  
+APIs Used: `discord.py` and `pokebase`
+
+### Commands
+- `,p view {pokemon name}` - Displays information about the pokemon (e.g. `,p view pikachu`)
+
+
+### Running The Bot
+- Download `main.py`
+- Make a bot in the discord developer portal
+- Make a file called `.env` and copy your token into it - Format: `TOKEN=your_token_here`
+- Make sure the `main.py` and `.env` files are in the same folder
+- Run `main.py`
---
+++ TwoAPIs/main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext.commands import Bot
+from discord.utils import get
+from discord.ext import commands
+
+import pokebase as pb
+
+import os
+from dotenv import load_dotenv
+
+#-----------------------------MAIN------------------------------#
+bot = commands.Bot(command_prefix=",p ", intents=discord.Intents.default())
+
+@bot.command(pass_context=True,aliases=["v"])
+async def view(ctx, pname):
+	p = pb.pokemon(pname.lower())
+	
+	abilities = ""
+	for i in range(len(p.abilities)):
+		abilities += str(i+1)+" - `"+p.abilities[i].ability.name+"`\n"
+	
+	embed = discord.Embed(title=f"{p.id} - {p.name}", color=0xfcfc14)
+	embed.set_thumbnail(url=p.sprites.front_default)
+	embed.add_field(name="General Info", value=f"Type: `{p.types[0].type.name}`\nHeight: `{p.height}`\tWeight: `{p.weight}`", inline=False)
+	embed.add_field(name="Stats", value=f"HP: `{p.stats[0].base_stat}`\nATK/DEF: `{p.stats[1].base_stat}/{p.stats[2].base_stat}`\nSP ATK/SP DEF: `{p.stats[3].base_stat}/{p.stats[4].base_stat}`\nSpeed: `{p.stats[5].base_stat}`", inline=False)
+	embed.add_field(name="Abilities", value=f"{abilities}", inline=False)
+	await ctx.message.channel.send(embed=embed)
+
+
+#-------------------------TOKEN---------------------------------#
+load_dotenv()
+bot.run(os.getenv("TOKEN"))
